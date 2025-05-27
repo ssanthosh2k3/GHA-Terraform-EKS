077e85c6833e7bfaa8cf6a9cdb408c55b4e2fe9d
santa <ssanthosh2k3@gmail.com>
2025-05-27 06:39:40 +0000

    backend modify

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
     bucket = "santhosh-eks-tf"
-    key    = "dev/terraform.tfstate"
+    key    = "terraform/state/dev/terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
-    # optional: dynamodb_table = "terraform-lock-table"  # if you want locking
   }
 }
 
